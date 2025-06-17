23f090327de876ee9ee9018490bc5032165bab79
David Zhang <david@cosyne.com>
2025-06-17 20:39:20 +0100

    Revert "refactor: to use faer"
    This reverts commit fa30631ea899eb292e424c3fbd6257eb2677f428.

--- src/rust/src/fun_nmf.rs
+++ src/rust/src/fun_nmf.rs
@@ -1,42 +1,32 @@
+use ndarray::{array, Array1, Array2, Axis, s};
+use ndarray_linalg::Solve;
 
-use faer::{Mat, mat};
+fn blockpivot(m1: Array2<f64>, m2: Array2<f64>) {
+    let m1_t_m1 = m1.t().dot(&m1);
+    let m1_t_m2 = m1.t().dot(&m2);
 
-fn blockpivot(m1: Mat<f64>, m2: Mat<f64>) {
-    let m1_t_m1 = m1.transpose() * &m1;
-    let m1_t_m2 = m1.transpose() * &m2;
 
-    let (n, k) = m1_t_m2.shape();
+    let shape = m1_t_m2.shape();
+    let (n, k) = (shape[0], shape[1]);
     let max_iter = n * 5;
+    
+    let x = Array2::<f64>::zeros((n, k));
+    let y = -m1_t_m2.clone();
 
-    let x = Mat::<f64>::zeros(n, k);
-    let y = -m1_t_m2;
-
-    // Use vector as faer only supports f64/f32 matrices.
-    let mut pass_set = vec![vec![false; k]; n];
 
     let p_bar = 3;
-    let mut p_vec = vec![p_bar; k];
-    let mut ninf_vec = vec![(n + 1) as i32; k];
+    let mut pass_set = Array2::<bool>::from_elem((n, k), false);
+    let mut p_vec = Array1::<u32>::from_elem( k, p_bar);
+    let mut ninf_vec = Array1::<u32>::from_elem( k, n as u32 + 1);
 
-    let mut not_opt_set =  vec![vec![false; k]; n];
-    for i in 0..n {
-        for j in 0..k {
-            not_opt_set[i][j] = y[(i, j)] < 0.0
-        }
-    }
-    let infea_set = vec![vec![false; k]; n];
+    let not_opt_set = y.map(|x| *x < 0.0);
+    let infea_set = Array2::<bool>::from_elem((n, k), false);
 
-    let mut not_good = vec![0 as i32; k];
-    for row in not_opt_set {
-        for (j, &val) in row.iter().enumerate() {
-            if val {
-                not_good[j] += 1;
-            }
-        }
-    }
-    let not_opt_colset: Vec<bool> = not_good.iter().map(|&x| x > 0).collect();
+    let not_good = not_opt_set.mapv(|x|x as u32).sum_axis(Axis(0)); 
+
+    let not_opt_colset  = not_good.map(|x| *x > 0);
     let not_opt_cols: Vec<usize> = non_zero_indexes(&not_opt_colset);
-
+        
     let mut big_iter = 0;
     let mut success = true;
 
@@ -49,52 +39,29 @@ fn blockpivot(m1: Mat<f64>, m2: Mat<f64>) {
             break
         }
 
-        let not_good_le_ninf= &not_good
-            .iter()
-            .zip(ninf_vec.iter())
-            .map(|(&x, &y)| x < y)
-            .collect::<Vec<bool>>();
+        let cols_set_1 = &not_opt_colset & (
+            &not_good.iter()
+                .zip(ninf_vec.iter())
+                .map(|(&x, &y)| x < y)
+                .collect::<Array1<bool>>()
+            );
 
-        let not_good_gr_ninf = &not_good
-            .iter()
-            .zip(ninf_vec.iter())
-            .map(|(&x, &y)| x >= y)
-            .collect::<Vec<bool>>();
+        let temp_1 = &not_opt_colset & (
+            &not_good.iter()
+                .zip(ninf_vec.iter())
+                .map(|(&x, &y)| x >= y)
+                .collect::<Array1<bool>>()
+            );
 
-        let cols_set_1 = &not_opt_colset
-            .iter()
-            .zip(not_good_le_ninf)
-            .map(|(&x, &y)| x & y)
-            .collect::<Vec<bool>>();
+        let temp_2 = p_vec.map(|x| *x >= 1);
 
-        let temp_1 = &not_opt_colset
-            .iter()
-            .zip(not_good_gr_ninf)
-            .map(|(&x, &y)| x & y)
-            .collect::<Vec<bool>>();
+        let cols_set_2 = &temp_1 & &temp_2;
+        let cols_set_3 = &temp_1 & &temp_2.map(|x| !x);
 
-        let temp_2 = p_vec.iter().map(|&x| x >= 1).collect::<Vec<bool>>();
+        let cols_1 = non_zero_indexes(&cols_set_1);
+        let cols_2 = non_zero_indexes(&cols_set_2);
+        let cols_3 = non_zero_indexes(&cols_set_3);
 
-        let cols_set_2 = &temp_1
-            .iter()
-            .zip(&temp_2)
-            .map(|(&x, y)| x & y)
-            .collect::<Vec<bool>>();
-
-        let cols_set_3 = &temp_1
-            .iter()
-            .zip(&temp_2)
-            .map(|(&x, y)| x & !y)
-            .collect::<Vec<bool>>();
-       
-        let cols_1 = non_zero_indexes(cols_set_1);
-        let cols_2 = non_zero_indexes(cols_set_2);
-        let cols_3 = non_zero_indexes(cols_set_3);
-
-        println!("{:?}", cols_1);
-        println!("{:?}", cols_2);
-        println!("{:?}", cols_3);
- 
         if !cols_1.is_empty() {
             for &i in &cols_1 {
                 p_vec[i] = p_bar;
@@ -118,7 +85,6 @@ fn blockpivot(m1: Mat<f64>, m2: Mat<f64>) {
 
         }
         
-        /* 
         if !cols_2.is_empty() {
             for &i in &cols_2 {
                 p_vec[i] = p_vec[i] - 1;
@@ -156,19 +122,19 @@ fn blockpivot(m1: Mat<f64>, m2: Mat<f64>) {
         let m1_t_m2_slice = m1_t_m2.select(Axis(1), &not_opt_cols);
         println!("m1_t_m1 shape: {:?}", m1_t_m1.shape());
         println!("m1_t_m2_slice shape: {:?}", m1_t_m2_slice.shape());
+        // let Z = AtA.solve_into(m1_t_m2_slice).unwrap();
 
-
-        */
     } 
 }
 
-fn non_zero_indexes(bool_vec: &Vec<bool>) -> Vec<usize> {
-    bool_vec
+fn non_zero_indexes(array: &Array1<bool>) -> Vec<usize> {
+    array
         .iter()
         .enumerate()
         .filter_map(|(i, &not_opt)| if not_opt { Some(i) } else { None })
         .collect::<Vec<usize>>()
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -176,13 +142,13 @@ mod tests {
 
     #[test]
     fn test_group_cols() {
-        let a = mat![
+        let a = array![
             [0.0, 1.0, 2.0],
             [9.0, 7.0, 6.0],
             [3.0, 4.0, 5.0],
         ];
 
-        let w = mat![
+        let w = array![
             [3.0, 1.0],
             [5.0, 3.0],
             [2.0, 4.0]
