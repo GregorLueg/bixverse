3a18b66a740498b98c3a045e1070744a2347be3d
David Zhang <32676710+dzhang32@users.noreply.github.com>
2025-06-24 09:56:58 +0100

    feat: wrap up ndarray blockpivot implementation

--- src/rust/src/fun_nmf.rs
+++ src/rust/src/fun_nmf.rs
@@ -1,40 +1,29 @@
+use ndarray::{array, Array1, Array2, Axis, s, Ix1};
+use ndarray_linalg::Solve;
 
-use faer::{Mat, mat};
+fn blockpivot(m1: Array2<f64>, m2: Array2<f64>) -> Array2<f64> {
+    let m1_t_m1 = m1.t().dot(&m1);
+    let m1_t_m2 = m1.t().dot(&m2);
 
-fn blockpivot(m1: Mat<f64>, m2: Mat<f64>) {
-    let m1_t_m1 = m1.transpose() * &m1;
-    let m1_t_m2 = m1.transpose() * &m2;
-
-    let (n, k) = m1_t_m2.shape();
+    let shape = m1_t_m2.shape();
+    let (n, k) = (shape[0], shape[1]);
     let max_iter = n * 5;
 
-    let x = Mat::<f64>::zeros(n, k);
-    let y = -m1_t_m2;
-
-    // Use vector as faer only supports f64/f32 matrices.
-    let mut pass_set = vec![vec![false; k]; n];
+    let mut x = Array2::<f64>::zeros((n, k));
+    // TODO - do we need to clone here?
+    let mut y = -m1_t_m2.clone();
 
     let p_bar = 3;
-    let mut p_vec = vec![p_bar; k];
-    let mut ninf_vec = vec![(n + 1) as i32; k];
+    let mut pass_set = Array2::<bool>::from_elem((n, k), false);
+    let mut p_vec = Array1::<u32>::from_elem( k, p_bar);
+    let mut ninf_vec = Array1::<u32>::from_elem( k, n as u32 + 1);
 
-    let mut not_opt_set =  vec![vec![false; k]; n];
-    for i in 0..n {
-        for j in 0..k {
-            not_opt_set[i][j] = y[(i, j)] < 0.0
-        }
-    }
-    let infea_set = vec![vec![false; k]; n];
+    let not_opt_set = y.map(|x| *x < 0.0);
+    let infea_set = Array2::<bool>::from_elem((n, k), false);
 
-    let mut not_good = vec![0 as i32; k];
-    for row in &not_opt_set {
-        for (j, &val) in row.iter().enumerate() {
-            if val {
-                not_good[j] += 1;
-            }
-        }
-    }
-    let not_opt_colset: Vec<bool> = not_good.iter().map(|&x| x > 0).collect();
+    let not_good = not_opt_set.mapv(|x|x as u32).sum_axis(Axis(0)); 
+
+    let not_opt_colset  = not_good.map(|x| *x > 0);
     let not_opt_cols: Vec<usize> = non_zero_indexes(&not_opt_colset);
 
     let mut big_iter = 0;
@@ -49,140 +38,117 @@ fn blockpivot(m1: Mat<f64>, m2: Mat<f64>) {
             break
         }
 
-        let not_good_le_ninf= &not_good
-            .iter()
-            .zip(ninf_vec.iter())
-            .map(|(&x, &y)| x < y)
-            .collect::<Vec<bool>>();
+        let cols_set_1 = &not_opt_colset & (
+            &not_good.iter()
+                .zip(ninf_vec.iter())
+                .map(|(&x, &y)| x < y)
+                .collect::<Array1<bool>>()
+            );
+        
 
-        let not_good_gr_ninf = &not_good
-            .iter()
-            .zip(ninf_vec.iter())
-            .map(|(&x, &y)| x >= y)
-            .collect::<Vec<bool>>();
+        let temp_1 = &not_opt_colset & (
+            &not_good.iter()
+                .zip(ninf_vec.iter())
+                .map(|(&x, &y)| x >= y)
+                .collect::<Array1<bool>>()
+            );
 
-        let cols_set_1 = &not_opt_colset
-            .iter()
-            .zip(not_good_le_ninf)
-            .map(|(&x, &y)| x & y)
-            .collect::<Vec<bool>>();
+        let temp_2 = p_vec.map(|x| *x >= 1);
 
-        let temp_1 = &not_opt_colset
-            .iter()
-            .zip(not_good_gr_ninf)
-            .map(|(&x, &y)| x & y)
-            .collect::<Vec<bool>>();
+        let cols_set_2 = &temp_1 & &temp_2;
+        let cols_set_3 = &temp_1 & &temp_2.map(|x| !x);
 
-        let temp_2 = p_vec.iter().map(|&x| x >= 1).collect::<Vec<bool>>();
+        let cols_1 = non_zero_indexes(&cols_set_1);
+        let cols_2 = non_zero_indexes(&cols_set_2);
+        let cols_3 = non_zero_indexes(&cols_set_3);
 
-        let cols_set_2 = &temp_1
-            .iter()
-            .zip(&temp_2)
-            .map(|(&x, y)| x & y)
-            .collect::<Vec<bool>>();
-
-        let cols_set_3 = &temp_1
-            .iter()
-            .zip(&temp_2)
-            .map(|(&x, y)| x & !y)
-            .collect::<Vec<bool>>();
-       
-        let cols_1 = non_zero_indexes(cols_set_1);
-        let cols_2 = non_zero_indexes(cols_set_2);
-        let cols_3 = non_zero_indexes(cols_set_3);
- 
         if !cols_1.is_empty() {
             for &i in &cols_1 {
                 p_vec[i] = p_bar;
                 ninf_vec[i] = not_good[i];
             }
 
-            let cols_set_1: Vec<Vec<bool>> = (0..n).map(|_| cols_set_1.clone()).collect();
-            
-            let true_set = elementwise_and(&not_opt_set, &cols_set_1);
-            let false_set = elementwise_and(&infea_set, &cols_set_1);
+            let cols_set_1 = cols_set_1.broadcast(not_opt_set.shape()).unwrap().to_owned();
 
-            let rows = true_set.len();
-            let cols = true_set[0].len();
-            for i in 0..rows {
-                for j in 0..cols {
-                    if true_set[i][j] {
-                        pass_set[i][j] = true;
-                    }
-                    if false_set[i][j] {
-                        pass_set[i][j] = false;
-                    }
-                }
-            }
+            let true_set = &not_opt_set & &cols_set_1;
+            let false_set = &infea_set & &cols_set_1;
+
+            true_set
+                .iter()
+                .zip(pass_set.iter_mut())
+                .for_each(|(&b, p)| if b { *p = true; });
+
+            false_set
+                .iter()
+                .zip(pass_set.iter_mut())
+                .for_each(|(&b, p)| if b { *p = false; });
 
         }
-
+        
         if !cols_2.is_empty() {
             for &i in &cols_2 {
                 p_vec[i] -= 1;
             }
 
-            let cols_set_2: Vec<Vec<bool>> = (0..n).map(|_| cols_set_1.clone()).collect();
+            let cols_set_2 = cols_set_2.broadcast(not_opt_set.shape()).unwrap().to_owned();
 
-            let true_set = elementwise_and(&not_opt_set, &cols_set_2);
-            let false_set = elementwise_and(&infea_set, &cols_set_2);
+            let true_set = &not_opt_set & &cols_set_2;
+            let false_set = &infea_set & &cols_set_2;
 
-            let rows = true_set.len();
-            let cols = true_set[0].len();
-            for i in 0..rows {
-                for j in 0..cols {
-                    if true_set[i][j] {
-                        pass_set[i][j] = true;
-                    }
-                    if false_set[i][j] {
-                        pass_set[i][j] = false;
-                    }
-                }
-            }
-        }
+            true_set
+                .iter()
+                .zip(pass_set.iter_mut())
+                .for_each(|(&b, p)| if b { *p = true; });
+
+            false_set
+                .iter()
+                .zip(pass_set.iter_mut())
+                .for_each(|(&b, p)| if b { *p = false; });
+
+        } 
         
         if !cols_3.is_empty() {
             for &col in &cols_3 {
-                let mut candi_set = vec![false; n];
-                for i in 0..not_opt_set.len() {
-                    if &not_opt_set[i][col] | &infea_set[i][col] {
-                        candi_set[i] = true;
-                    }
-                }
-
+                let candi_set= &not_opt_set.column(col) | &infea_set.column(col);
                 let to_change = *non_zero_indexes(&candi_set).iter().max().unwrap();
-                pass_set[to_change][col] = !pass_set[to_change][col];
+
+                pass_set[(to_change, col)] = !pass_set[(to_change, col)];
             }
+
         }
 
+
+        for i in &not_opt_cols {
+           x.column_mut(*i).assign(&m1_t_m1.solve(&m1_t_m2.column(*i)).unwrap());
+        }
+
+        let to_update_y = m1_t_m1.dot(&x.select(Axis(1), &not_opt_cols)) - m1_t_m2.select(Axis(1), &not_opt_cols);
+        for (i, col) in not_opt_cols.iter().enumerate() {
+            y.column_mut(*col).assign(&to_update_y.column(i));
+        }
         
-        //let m1_t_m2_slice = m1_t_m2.select(Axis(1), &not_opt_cols);
-
-
+        let not_opt_mask = not_opt_colset.broadcast(not_opt_set.shape()).unwrap().to_owned();
+        let not_opt_set = (&not_opt_mask & &y.map(|x| *x < 0.0)) & !&pass_set;
+        let infea_set = (&not_opt_mask & &x.map(|x| *x < 0.0)) & &pass_set;
+        
+        let not_good = not_opt_set.mapv(|b| b as u32).sum_axis(Axis(0)) + infea_set.mapv(|b| b as u32).sum_axis(Axis(0));
+        let not_good = not_good.into_dimensionality::<Ix1>().unwrap();
+        let not_opt_colset = not_good.map(|x| *x > 0); 
+        let not_opt_cols = non_zero_indexes(&not_opt_colset);
+        
     } 
+
+    x
 }
 
-fn non_zero_indexes(bool_vec: &Vec<bool>) -> Vec<usize> {
-    bool_vec
+fn non_zero_indexes(array: &Array1<bool>) -> Vec<usize> {
+    array
         .iter()
         .enumerate()
         .filter_map(|(i, &not_opt)| if not_opt { Some(i) } else { None })
         .collect::<Vec<usize>>()
 }
 
-fn elementwise_and(a: &Vec<Vec<bool>>, b: &Vec<Vec<bool>>) -> Vec<Vec<bool>> {
-    let result: Vec<Vec<bool>> = a.iter()
-    .zip(b.iter())
-    .map(|(row_a, row_b)| {
-        row_a.iter()
-            .zip(row_b.iter())
-            .map(|(x, y)| *x && *y)
-            .collect()
-    })
-    .collect();
-
-    result
-}
 
 #[cfg(test)]
 mod tests {
@@ -190,22 +156,35 @@ mod tests {
 
     #[test]
     fn test_group_cols() {
-        let a = mat![
+        let a = array![
             [0.0, 1.0, 2.0],
             [9.0, 7.0, 6.0],
             [3.0, 4.0, 5.0],
         ];
 
-        let w = mat![
+        let w = array![
             [3.0, 1.0],
             [5.0, 3.0],
             [2.0, 4.0]
         ];
 
-        let mat = Mat::<f64>::from_fn(4, 5, |i, j| (i * 10 + j) as f64);
-        let submat: MatRef<'_, f64> = mat.as_ref().subcols(1, 2);
-        println!("mat: {:?}", mat);
-        //blockpivot(w, a);
 
+        // Expected output derived from python blockpivot implementation:
+        // https://github.com/kimjingu/nonnegfac-python/blob/master/nonnegfac/nnls.py
+        let expected_output = array![
+            [1.0, 0.66666667, 0.5],
+            [0.5, 0.79487179, 1.03846154]
+        ];
+
+
+        let output= blockpivot(w, a);
+
+        // Check output is very close to expected output.
+        let tol = 1e-5;
+        assert!(
+            output.iter()
+            .zip(expected_output.iter())
+            .all(|(x, y)| (*x - *y).abs() < tol)
+        );
     }
 }
